aaefacac3ed54ccaf463ea5183cb846d65f25137
marssuren <287815741@qq.com>
2018-05-07 20:09:33 +0800

    Initial commit

---
+++ Assets/Scripts/Trace.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//像弹簧一样跟随目标物体
+public class Trace : MonoBehaviour 
+{
+	public Transform Target;
+	public float Smooth=5.0f;
+	void Update()
+	{
+		transform.position=Vector3.Lerp(transform.position,Target.position,Time.deltaTime*Smooth);
+	}
+
+}
